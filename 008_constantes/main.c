b662f2983c980dc18b3f5686cb642bbf51973ebe
Bernardo <mrochabernardo@gmail.com>
2022-07-03 10:54:18 -0300

    exercicios

--- 008_constantes/main.c
+++ 008_constantes/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define PI 3.141592
 int main()
 {
-    printf("Hello world!\n");
+    printf("Constantes #define e/ou const\n");
+    printf("PI = %f", PI);
+    //const float PI = 3.141592;
     return 0;
 }
---
+++ 009_sequenciasEscape/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*
+    printf("Hello world!\n");
+    printf("Hello\nworld!\n");
+    printf("Hello \\ world!\n");
+    printf("\"Hello world\"\n");
+
+    printf("Ex01\n");
+    printf("Início do programa.\nFim");
+    printf("\n\n");
+
+    printf("Ex02 - Ler um número inteiro e depois imprimir: \n");
+    int x;
+    scanf("%d", &x);
+    printf("O número digitado foi: %d\n", x);
+    printf("\n\n");
+
+    printf("Ex03\n");
+    int exe3;
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &exe3);
+    printf("Valor lido: %d", exe3);
+    printf("\n\n");
+
+    printf("Ex04\n");
+    int exe4;
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &exe4);
+    printf("Valor digitado %f", exe4);
+    printf("\n\n");
+*/
+    printf("Ex05\n");
+    float flut5;
+    printf("Digite um valor de ponto flutuante: ");
+    scanf("%f", &flut5);
+    printf("Valor digitado: %d", flut5);
+
+    system("pause");
+    return 0;
+}
