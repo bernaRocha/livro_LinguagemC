76579418b65c42d2e67ce578a3931b428709f00d
Bernardo <mrochabernardo@gmail.com>
2022-07-04 18:00:38 -0300

    Revert "arquivo de exercicios separado"
    This reverts commit 213d03644b3e413a334cfe82bc5e9786259eafc7.

--- 009_sequenciasEscape/main.c
+++ 009_sequenciasEscape/main.c
@@ -39,7 +39,8 @@ int main()
     printf("Digite um valor de ponto flutuante: ");
     scanf("%f", &flut5);
     printf("Valor digitado: %d", flut5); //Digitei 1.0 e imprimiu 1065353216
-
+    printf("Ex06\n");
+*/
     printf("Ex06\n");
     printf("Leia um tipo double e o imprima na forma de notação científica\n");
     double d1;
@@ -47,26 +48,6 @@ int main()
     scanf("%lf", &d1);
     printf("O valor digitado é: %f\n", d1);
     printf("Em notação científica fica %.1E", d1);
-
-    printf("Ex07 - Ler um caractere e imprimir como número inteiro.\n");
-    char letra;
-    printf("Digite a letra: \n");
-    scanf("%c", &letra);
-    printf("A letra digita %c tem o valor inteiro: %i", letra, letra);
-*/
-    printf("Ex08 Ler três números e imprimir na ordem inversa. \n");
-    int i, numbers[3];
-    printf("Digite três números inteiros: \n");
-
-    for(i = 0; i < 3; i++) {
-        printf("Digite o valor %d: ", i + 1);
-        scanf("%d", &numbers[i]);
-            fflush(stdin);
-    }
-    printf("Resultado: \n");
-    for(i = 2; i >= 0; i--) {
-        printf("%d ", numbers[i]);
-    }
     //system("pause");
     return 0;
 }
