17b78fb61f892d954ff4a8be7dc365f723e2301c
Bernardo <mrochabernardo@gmail.com>
2022-08-01 08:48:31 -0300

    ex do 1 ao 3 feitos

--- 015_exercicios_cap3/main.c
+++ 015_exercicios_cap3/main.c
@@ -4,7 +4,7 @@
 int main()
 {
     printf("Exercícios do cap 3: \n");
-
+/*
     int inteiro, antecessor, sucessor;
 
     printf("Ex 01 - Ler um número inteiro e retornar o antecessor e o seu sucessor.\n");
@@ -16,6 +16,21 @@ int main()
 
     printf("Número registrado é: %d\n", inteiro);
     printf("O antecessor de %d é %d e o seu sucessor é %d.", inteiro, antecessor, sucessor);
+    printf("Ex 02 - ler um número real e imprimir a quinta parte dele.\n");
+    int real, quintaParte;
+    printf("Registre o número: ");
+    scanf("%d", &real);
+    quintaParte = real / 5;
+    printf("A quinta parte de %d é %d", real,quintaParte);
+*/
 
+    printf("Ex 03 - ler 3 inteiros e imprimir a soma. \n");
+    int num1, num2, num3, soma;
+    printf("Registre os números para ver a soma deles.\n");
+
+    scanf("%d%d%d", &num1, &num2, &num3);
+    soma = num1 + num2 + num3;
+
+    printf("A soma de %d, %d, %d é %d", num1, num2, num3, soma);
     return 0;
 }
